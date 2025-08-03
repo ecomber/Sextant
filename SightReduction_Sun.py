445cfd8c96e39ce4b2aa7e13092a46bc6a6e9612
ecomber <e@comber.org>
2025-08-03 21:25:55 +0100

    Table formatting

--- SightReduction_Sun.py
+++ SightReduction_Sun.py
@@ -13,31 +13,31 @@ CP_longitude_degrees = -9
 
 observation_time = datetime.datetime(2025, 5, 29, 15, 19, 28, 914876, ZoneInfo('UTC'))
 
-GHA_of_body_at_hour = (45 + 38.1/60) # GHA of Sun on the hour from Almanac (deg + min), increment is calculated
-Declination_of_body_at_hour = (21 + 43.2/60) # of Observed Body, from Almanac.
-Declination_d_correction = 0.4 # N.B. sign. + ve 21 December to 21 June for Sun, from Almanac.
+GHA_of_body_at_hour = (45 + 38.1 / 60)  # GHA of Sun on the hour from Almanac (deg + min), increment is calculated
+Declination_of_body_at_hour = (21 + 43.2 / 60)  # of Observed Body, from Almanac.
+Declination_d_correction = 0.4  # N.B. sign. + ve 21 December to 21 June for Sun, from Almanac.
 # GHA and Decl increments are calculated (GHA at 15°/hr) and (Decl at 'd' * minutes)
 # Enter Sun semi-diameter below
 
 mySextant = sextant.Sextant()
-mySextant.Hs = 50 + 58.1 /60 # uncorrected sextant reading (deg + min)
-mySextant.index_error= +2.2 # (On - / Off +)
-mySextant.eye_height = 2.5 # Cockpit = 2.5 Mast = 3.3
-mySextant.semi_diameter = +15.8 # Varies, get from Almanac. negative for upper limb
+mySextant.Hs = 50 + 58.1 / 60  # uncorrected sextant reading (deg + min)
+mySextant.index_error = +2.2  # (On - / Off +)
+mySextant.eye_height = 2.5  # Cockpit = 2.5 Mast = 3.3
+mySextant.semi_diameter = +15.8  # Varies, get from Almanac. negative for upper limb
 
 # end of observation data
 ac = astroCalculator.AstroCalculator()
 mySextant.Calculate()
 Ho = mySextant.Ho()
 
-GHA_m_s_increment = observation_time.minute / 4 + observation_time.second / 240 # e.g. 4 arc' in 1 minute of time
+GHA_m_s_increment = observation_time.minute / 4 + observation_time.second / 240  # e.g. 4 arc' in 1 minute of time
 GHA_of_body_degrees = GHA_of_body_at_hour + GHA_m_s_increment
 
 LHA_of_body_degrees = (GHA_of_body_degrees + CP_longitude_degrees + 360) % 360
-Declination_m_increment = observation_time.minute * Declination_d_correction/3600 # specified in minutes of arc
+Declination_m_increment = observation_time.minute * Declination_d_correction / 3600  # specified in minutes of arc
 Declination_of_body_degrees = Declination_of_body_at_hour + Declination_m_increment
 
-Z, Hc = ac.sightReduction(CP_latitude_degrees, Declination_of_body_degrees, LHA_of_body_degrees) # Z later corrected to Zn
+Z, Hc = ac.sightReduction(CP_latitude_degrees, Declination_of_body_degrees, LHA_of_body_degrees)  # Z later corrected to Zn
 
 observed_body = "Sun"
 
@@ -49,71 +49,71 @@ infoTable.append(f"Calculation time {str(datetime.datetime.now(ZoneInfo('UTC')))
 infoTable.append("")
 
 outputTable = []
-outputTable.append(["",""])
+outputTable.append(["", ""])
 outputTable.append([f"Position of {observed_body}:", ""])
-outputTable.append([f"GHA of {observed_body} {observation_time.year}-{observation_time.month:02g}-{observation_time.day:02g} {observation_time.hour:02g}h (Almanac)",f"{deg_to_dm(GHA_of_body_at_hour%360)}", GHA_of_body_at_hour])
-outputTable.append([f"GHA increment at {observation_time.minute}m {observation_time.second}s ",f"{deg_to_dm(GHA_m_s_increment)}"])
-outputTable.append([f"GHA of {observed_body} at {observation_time.hour}h {observation_time.minute}m {observation_time.second}s",f"{deg_to_dm(GHA_of_body_degrees%360)}", GHA_of_body_degrees])
-outputTable.append([f"Declination at {observation_time.hour}h (Almanac)",f"{deg_to_dm(Declination_of_body_at_hour)}"])
-outputTable.append([f"Decl incr {Declination_d_correction} at {observation_time.minute}m (Almanac)",f"{deg_to_dm(Declination_m_increment)}"])
-outputTable.append([f"Declination of {observed_body}",f"{deg_to_dm(Declination_of_body_degrees)}",Declination_of_body_degrees])
+outputTable.append([f"GHA of {observed_body} {observation_time.year}-{observation_time.month:02g}-{observation_time.day:02g} {observation_time.hour:02g}h (Almanac)", f"{deg_to_dm(GHA_of_body_at_hour % 360)}",GHA_of_body_at_hour])
+outputTable.append([f"GHA increment at {observation_time.minute}m {observation_time.second}s ", f"{deg_to_dm(GHA_m_s_increment)}"])
+outputTable.append([f"GHA of {observed_body} at {observation_time.hour}h {observation_time.minute}m {observation_time.second}s", f"{deg_to_dm(GHA_of_body_degrees % 360)}", GHA_of_body_degrees])
+outputTable.append([f"Declination at {observation_time.hour}h (Almanac)", f"{deg_to_dm(Declination_of_body_at_hour)}"])
+outputTable.append([f"Decl incr {Declination_d_correction} at {observation_time.minute}m (Almanac)", f"{deg_to_dm(Declination_m_increment)}"])
+outputTable.append([f"Declination of {observed_body}", f"{deg_to_dm(Declination_of_body_degrees)}", Declination_of_body_degrees])
 
-outputTable.append(["",""])
-outputTable.append([f"Chosen Position:",""])
-outputTable.append([f"CP Latitude",f"{deg_to_dm(CP_latitude_degrees)}", CP_latitude_degrees])
-outputTable.append([f"CP Longitude",f"{deg_to_dm(CP_longitude_degrees)}", CP_longitude_degrees])
-outputTable.append([f"LHA of {observed_body} (GHA + CP Lon)",f"{deg_to_dm(LHA_of_body_degrees)}", LHA_of_body_degrees])
+outputTable.append(["", ""])
+outputTable.append([f"Chosen Position:", ""])
+outputTable.append([f"CP Latitude", f"{deg_to_dm(CP_latitude_degrees)}", CP_latitude_degrees])
+outputTable.append([f"CP Longitude", f"{deg_to_dm(CP_longitude_degrees)}", CP_longitude_degrees])
+outputTable.append([f"LHA of {observed_body} (GHA + CP Lon)", f"{deg_to_dm(LHA_of_body_degrees)}", LHA_of_body_degrees])
 
-outputTable.append(["",""])
-outputTable.append([f"Hc calculated from CP lat, Decl, LHA",f"{deg_to_dm(Hc)}",Hc])
-outputTable.append([f"Ho from sextant",f"{deg_to_dm(Ho)}",Ho])
+outputTable.append(["", ""])
+outputTable.append([f"Hc calculated from CP lat, Decl, LHA", f"{deg_to_dm(Hc)}", Hc])
+outputTable.append([f"Ho from sextant", f"{deg_to_dm(Ho)}", Ho])
 
 if Ho > Hc:
-	direction = "Towards"
+    direction = "Towards"
 else:
-	direction = "Away"
+    direction = "Away"
 
-outputTable.append([f"Intercept ({direction})",f"{deg_to_dm(Hc-Ho)}"])
-outputTable.append([f"Intercept ({direction})",f"{abs((Hc-Ho)*60):5.2f} nm "])
+outputTable.append([f"Intercept ({direction})", f"{deg_to_dm(Hc - Ho)}"])
+outputTable.append([f"Intercept ({direction})", f"{abs((Hc - Ho) * 60):5.2f} nm "])
 
-if CP_latitude_degrees > 0: # North
-	#outputTable.append(["Latitude North",""])
-	if LHA_of_body_degrees > 180:
-		#outputTable.append([" and LHA > 180°: Zn = Z",""])
-		Zn = Z
-	else:
-		#outputTable.append([" and LHA < 180°: Zn = 360° - Z",""])
-		Zn = 360 - Z
-else: # South
-	outputTable.append(["Latitude South",""])
-	if LHA_of_body_degrees > 180:
-		outputTable.append([" and LHA > 180°: Zn = 180° - Z",""])
-		Zn = 180 - Z
-	else:
-		outputTable.append([" and LHA < 180°: Zn = 180° + Z",""])
-		Zn = 180 + Z
+if CP_latitude_degrees > 0:  # North
+    # outputTable.append(["Latitude North",""])
+    if LHA_of_body_degrees > 180:
+        # outputTable.append([" and LHA > 180°: Zn = Z",""])
+        Zn = Z
+    else:
+        # outputTable.append([" and LHA < 180°: Zn = 360° - Z",""])
+        Zn = 360 - Z
+else:  # South
+    outputTable.append(["Latitude South", ""])
+    if LHA_of_body_degrees > 180:
+        outputTable.append([" and LHA > 180°: Zn = 180° - Z", ""])
+        Zn = 180 - Z
+    else:
+        outputTable.append([" and LHA < 180°: Zn = 180° + Z", ""])
+        Zn = 180 + Z
 
-outputTable.append([f"  Zn (Azimuth) {round(Zn)}°, LoP {round((Zn+90)%360)}°",""])
+outputTable.append([f"  Zn (Azimuth) {round(Zn)}°, LoP {round((Zn + 90) % 360)}°", ""])
 
 for row in infoTable:
-	print(row)
-tables = [mySextant.formattedTable(),outputTable]
-column0_width = column1_width = 0 # get max column widths
+    print(row)
+tables = [mySextant.formattedTable(), outputTable]
+column0_width = column1_width = 0  # get max column widths
 for table in tables:
-	for row in table:
-		column0_width = max(len(row[0]), column0_width)
-		column1_width = max(len(row[1]), column1_width)
+    for row in table:
+        column0_width = max(len(row[0]), column0_width)
+        column1_width = max(len(row[1]), column1_width)
 for table in tables:
-	for row in table:
-		print(row[0].ljust(column0_width + 2), row[1].rjust(column1_width))
+    for row in table:
+        print(row[0].ljust(column0_width + 2), row[1].rjust(column1_width))
 
 if Logging is True:
-	OutFileName = "Logs/" + str(observation_time.strftime("%Y-%m-%d %Hh%Mm%Ss UTC")) + f" Sun.txt"
-	# ':' not permitted in filename
-	with open(OutFileName, "w", encoding="utf-8") as logfile:
-		for row in infoTable:
-			logfile.write(row + "\n")
-		for row in mySextant.formattedTable():
-			logfile.write(f"{row[0].ljust(column0_width + 2)}{row[1].rjust(column1_width)}\n")
-		for row in outputTable:
-			logfile.write(f"{row[0].ljust(column0_width + 2)}{row[1].rjust(column1_width)}\n")
+    OutFileName = "Logs/" + str(observation_time.strftime("%Y-%m-%d %Hh%Mm%Ss UTC")) + f" Sun.txt"
+    # ':' not permitted in filename
+    with open(OutFileName, "w", encoding="utf-8") as logfile:
+        for row in infoTable:
+            logfile.write(row + "\n")
+        for row in mySextant.formattedTable():
+            logfile.write(f"{row[0].ljust(column0_width + 2)}{row[1].rjust(column1_width)}\n")
+        for row in outputTable:
+            logfile.write(f"{row[0].ljust(column0_width + 2)}{row[1].rjust(column1_width)}\n")
--- sextant.py
+++ sextant.py
@@ -4,7 +4,7 @@ from zoneinfo import ZoneInfo
 
 
 def deg_to_dm(deg):
-	"""Converts decimal degrees to printable D°m.m string
+    """Converts decimal degrees to printable D°m.m string
 
     Parameters
     ----------
@@ -16,142 +16,133 @@ def deg_to_dm(deg):
     str :
     degrees as printable string
     """
-	if deg < 0:
-		sign = "-"
-	else:
-		sign = "+"
-	decimals, number = math.modf(deg)
-	d = int(number)
-	m = decimals * 60
-	#return f"{sign}{abs(d)}°{round(abs(m), 2):05.02f}'".rjust(8)
-	return f"{sign}{abs(d)}°{abs(m):05.02f}'".rjust(8)
+    if deg < 0:
+        sign = "-"
+    else:
+        sign = "+"
+    decimals, number = math.modf(deg)
+    d = int(number)
+    m = decimals * 60
+    # return f"{sign}{abs(d)}°{round(abs(m), 2):05.02f}'".rjust(8)
+    return f"{sign}{abs(d)}°{abs(m):05.02f}'".rjust(8)
 
 
 class Sextant(object):
 
-	def __init__(self):
-		self.index_error = 0
-		self.eye_height = 0
-		self.Hs = 0
-		self.semi_diameter = 0
-		self.jsonDict = {}
-		self.table = []
+    def __init__(self):
+        self.index_error = 0
+        self.eye_height = 0
+        self.Hs = 0
+        self.semi_diameter = 0
+        self.jsonDict = {}
+        self.table = []
 
-	def __str__(self):
-		#value, string = self.Calculate()
-		#return string
-		return str(self.table)
+    def __str__(self):
+        # value, string = self.Calculate()
+        # return string
+        return str(self.table)
 
-	def correction(self, x):
-		pass
-		# y =-0.0271 * x3  +0.585 * x2 -4.5737 X + 3.8811
-		#y = -0.0271 * pow(x, 3) + 0.585 * pow(x, 2) - 4.5737 * x + 3.8811
-		y = -0.0271 * x*x*x + 0.585 * x*x - 4.5737 * x + 3.8811
-		return y
-		
-	def correction_1(self, x):
-		y = (-0.027097902097902 * x*x*x) + (0.585039960039961 * x*x)  - (4.57367632367633 * x) + 3.8811188811189
-		return y
-		
-	def correction_2(self, x):
-		y = -0.0000006 * x*x*x*x + 0.0001 * x*x*x - 0.0052 * x*x - 0.0989 * x -1.096
-		return y
-		
-	def formattedTable(self):
-		#self.Calculate()
-		return self.table
+    def correction(self, x):
+        pass
+        # y =-0.0271 * x3  +0.585 * x2 -4.5737 X + 3.8811
+        # y = -0.0271 * pow(x, 3) + 0.585 * pow(x, 2) - 4.5737 * x + 3.8811
+        y = -0.0271 * x * x * x + 0.585 * x * x - 4.5737 * x + 3.8811
+        return y
 
-	def dip(self, eye_height):
-		# returns dip in degrees
-		return 1.76 * math.sqrt(eye_height) / 60
+    def correction_1(self, x):
+        y = (-0.027097902097902 * x * x * x) + (0.585039960039961 * x * x) - (4.57367632367633 * x) + 3.8811188811189
+        return y
 
-	def bennett(self, Ho):
-		# returns refraction correction in degrees
-		# Ho in degrees https://en.wikipedia.org/wiki/Atmospheric_refraction
-		return (1 / math.tan(math.radians(Ho + (7.31 / (Ho + 4.4))))) / 60
+    def correction_2(self, x):
+        y = -0.0000006 * x * x * x * x + 0.0001 * x * x * x - 0.0052 * x * x - 0.0989 * x - 1.096
+        return y
 
-	def Ho(self):
-		value, string = self.Calculate()
-		return value
+    def formattedTable(self):
+        # self.Calculate()
+        return self.table
 
-	def Calculate(self):
-		self.table.clear()
-		retStr = ""
-		self.jsonDict["TimeStampCode"] = datetime.datetime.now(ZoneInfo('UTC'))
-		self.jsonDict["TimeStampISO"] = str(datetime.datetime.now(ZoneInfo('UTC')))
-		self.jsonDict["Hs"] = self.Hs
+    def dip(self, eye_height):
+        # returns dip in degrees
+        return 1.76 * math.sqrt(eye_height) / 60
 
-		index_error = self.index_error / 60
-		semi_diameter = self.semi_diameter / 60  # +ve Lower Limb
+    def bennett(self, Ho):
+        # returns refraction correction in degrees
+        # Ho in degrees https://en.wikipedia.org/wiki/Atmospheric_refraction
+        return (1 / math.tan(math.radians(Ho + (7.31 / (Ho + 4.4))))) / 60
 
-		retStr += f"Hs                           {deg_to_dm(self.Hs)}\n"
-		self.table.append(["Sextant:", ""])
-		self.table.append(["Hs (Height by Sextant)", f"{deg_to_dm(self.Hs)}"])
+    def Ho(self):
+        value, string = self.Calculate()
+        return value
 
-		retStr += f"Index Error (On - / Off +)    {deg_to_dm(index_error)}\n"
-		self.table.append(
-		 [f"Index Error (On - / Off +)", f"{deg_to_dm(index_error)}"])
-		self.jsonDict["IndexError"] = self.index_error
-		Hs = self.Hs + index_error
-		retStr += f"Hs                           {deg_to_dm(Hs)}\n"
-		self.table.append(["Hs", f"{deg_to_dm(Hs)}"])
+    def Calculate(self):
+        self.table.clear()
+        retStr = ""
+        self.jsonDict["TimeStampCode"] = datetime.datetime.now(ZoneInfo('UTC'))
+        self.jsonDict["TimeStampISO"] = str(datetime.datetime.now(ZoneInfo('UTC')))
+        self.jsonDict["Hs"] = self.Hs
 
-		self.jsonDict["EyeHeight"] = self.eye_height
-		dip_of_horizon = self.dip(self.eye_height)
-		self.jsonDict["Dip"] = self.dip(self.eye_height)
-		retStr += f"Dip at {self.eye_height}m eye height        {deg_to_dm(-dip_of_horizon)}\n"
-		self.table.append(
-		 [f"Dip at eye height {self.eye_height}m", f"{deg_to_dm(-dip_of_horizon)}"])
-		Hs = Hs - dip_of_horizon
-		retStr += f"Hs                           {deg_to_dm(Hs)}\n"
-		self.table.append(["Hs", f"{deg_to_dm(Hs)}"])
+        index_error = self.index_error / 60
+        semi_diameter = self.semi_diameter / 60  # +ve Lower Limb
 
-		refraction_correction = self.bennett(Hs)
-		
-		retStr += f"Refraction at {deg_to_dm(Hs)}      {deg_to_dm(-refraction_correction)}\n"
-		self.jsonDict["RefractionCorrection"] = refraction_correction
-		self.table.append(
-		 [f"Refraction at {deg_to_dm(Hs)}", f"{deg_to_dm(-refraction_correction)}"])
-		Hs = Hs - refraction_correction
-		self.table.append(["Hs", f"{deg_to_dm(Hs)}"])
-		retStr += f"Semi-diameter (Almanac)                 {deg_to_dm(semi_diameter)}\n"
-		self.jsonDict["Semi-diameter (Almanac)"] = self.semi_diameter
-		if self.semi_diameter != 0:
-			self.table.append([f"Semi-diameter (Almanac)", f"{deg_to_dm(semi_diameter)}"])
-			Hs = Hs + self.semi_diameter / 60
-			retStr += f"Hs                           {deg_to_dm(Hs)}\n"
-			#self.table.append(["Hs", f"{deg_to_dm(Hs)}"])
-		Ho = Hs
-		retStr += f"Ho                           {deg_to_dm(Ho)}\n"
-		self.jsonDict["Ho (Height Observed|)"] = Ho
-		retStr += f"Ho decimal degrees           {Ho:0.6f}\n"
-		self.table.append(["Ho (Height Observed)", f"{deg_to_dm(Ho)}"])
-		#self.table.append(["Ho as decimal",str(Ho)])
-		return Ho, retStr
+        retStr += f"Hs                           {deg_to_dm(self.Hs)}\n"
+        self.table.append(["Sextant:", ""])
+        self.table.append(["Hs (Height by Sextant)", f"{deg_to_dm(self.Hs)}"])
+
+        retStr += f"Index Error (On - / Off +)    {deg_to_dm(index_error)}\n"
+        self.table.append([f"Index Error (On - / Off +)", f"{deg_to_dm(index_error)}"])
+        self.jsonDict["IndexError"] = self.index_error
+        Hs = self.Hs + index_error
+        retStr += f"Hs                           {deg_to_dm(Hs)}\n"
+        self.table.append(["Hs", f"{deg_to_dm(Hs)}"])
+
+        self.jsonDict["EyeHeight"] = self.eye_height
+        dip_of_horizon = self.dip(self.eye_height)
+        self.jsonDict["Dip"] = self.dip(self.eye_height)
+        retStr += f"Dip at {self.eye_height}m eye height        {deg_to_dm(-dip_of_horizon)}\n"
+        self.table.append([f"Dip at eye height {self.eye_height}m", f"{deg_to_dm(-dip_of_horizon)}"])
+        Hs = Hs - dip_of_horizon
+        retStr += f"Hs                           {deg_to_dm(Hs)}\n"
+        self.table.append(["Hs", f"{deg_to_dm(Hs)}"])
+
+        refraction_correction = self.bennett(Hs)
+
+        retStr += f"Refraction at {deg_to_dm(Hs)}      {deg_to_dm(-refraction_correction)}\n"
+        self.jsonDict["RefractionCorrection"] = refraction_correction
+        self.table.append([f"Refraction at {deg_to_dm(Hs)}", f"{deg_to_dm(-refraction_correction)}"])
+        Hs = Hs - refraction_correction
+        self.table.append(["Hs", f"{deg_to_dm(Hs)}"])
+        retStr += f"Semi-diameter (Almanac)                 {deg_to_dm(semi_diameter)}\n"
+        self.jsonDict["Semi-diameter (Almanac)"] = self.semi_diameter
+        if self.semi_diameter != 0:
+            self.table.append([f"Semi-diameter (Almanac)", f"{deg_to_dm(semi_diameter)}"])
+            Hs = Hs + self.semi_diameter / 60
+            retStr += f"Hs                           {deg_to_dm(Hs)}\n"  # self.table.append(["Hs", f"{deg_to_dm(Hs)}"])
+        Ho = Hs
+        retStr += f"Ho                           {deg_to_dm(Ho)}\n"
+        self.jsonDict["Ho (Height Observed|)"] = Ho
+        retStr += f"Ho decimal degrees           {Ho:0.6f}\n"
+        self.table.append(["Ho (Height Observed)", f"{deg_to_dm(Ho)}"])
+        # self.table.append(["Ho as decimal",str(Ho)])
+        return Ho, retStr
 
 
 if __name__ == '__main__':
-	pass
-	freib = Sextant()
-	freib.index_error = +2.1
-	freib.eye_height = 2.5
-	freib.Hs = 29 + 36.8 / 60
-	freib.semi_diameter = 15.8
-	freib.Calculate()
-	print(f"Sextant example output.")
-	print(f"Dip is 1.76 * sqrt(eye_height_meters) / 60")
-	print(
-	 f"Refraction is Bennett's formula https://en.wikipedia.org/wiki/Atmospheric_refraction"
-	)
-	print(
-	 f"Reported to be consistent within 0.07′ over the entire range from the zenith to the horizon.\n"
-	)
-	for row in freib.table:
-		print(row[0].ljust(28), row[1].rjust(12))
+    pass
+    freib = Sextant()
+    freib.index_error = +2.1
+    freib.eye_height = 2.5
+    freib.Hs = 29 + 36.8 / 60
+    freib.semi_diameter = 15.8
+    freib.Calculate()
+    print(f"Sextant example output.")
+    print(f"Dip is 1.76 * sqrt(eye_height_meters) / 60")
+    print(f"Refraction is Bennett's formula https://en.wikipedia.org/wiki/Atmospheric_refraction")
+    print(f"Reported to be consistent within 0.07′ over the entire range from the zenith to the horizon.\n")
+    for row in freib.table:
+        print(row[0].ljust(28), row[1].rjust(12))
 
-	#print(deg_to_dm( freib.correction(20)*3600))
-	with open("corrections.csv","w") as outfile:
-		outfile.write("a,b\n")
-		for i in range(0,13):
-			outfile.write(f"{i}, {freib.correction(i)}\n")
-
+    # print(deg_to_dm( freib.correction(20)*3600))
+    with open("corrections.csv", "w") as outfile:
+        outfile.write("a,b\n")
+        for i in range(0, 13):
+            outfile.write(f"{i}, {freib.correction(i)}\n")
